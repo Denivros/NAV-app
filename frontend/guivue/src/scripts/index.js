47262652ea394866d651311ee01e939b9ae26702
Denivros <45978934+Denivros@users.noreply.github.com>
2020-09-19 14:40:00 +0200

    dd

---
+++ frontend/guivue/src/scripts/index.js
@@ -0,0 +1,86 @@
+// // Firebase App (the core Firebase SDK) is always required and must be listed first
+// import firebase from "@firebase/app";
+
+// // If you enabled Analytics in your project, add the Firebase SDK for Analytics
+// import "@firebase/analytics";
+
+// // Add the Firebase products that you want to use
+// import "@firebase/auth";
+// import "@firebase/database";
+// import "@firebase/firestore";
+
+  // Set the configuration for your app
+  // TODO: Replace with your project's config object
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAEt3og515ZuYlivFWjrY00_rsgFLdXCuA",
+  authDomain: "nav-one-b8410.firebaseapp.com",
+  databaseURL: "https://nav-one-b8410.firebaseio.com",
+  projectId: "nav-one-b8410",
+  storageBucket: "nav-one-b8410.appspot.com",
+  messagingSenderId: "1068648920424",
+  appId: "1:1068648920424:web:4619d8376aa0eadc7692dd",
+  measurementId: "G-00X6NKNK5N"
+});
+firebase.analytics();
+
+const preObject = document.getElementById('object');
+const ulList = document.getElementById('list');
+// Get a reference to the database service
+const dbRefObject = firebase.database().ref().child('Aerodromes');
+const dbRefList = dbRefObject.child('Aerodromes');
+
+
+var ad_name = [];
+var icao = [];
+var iata = [];
+var coor = [];
+var elev = [];
+var type = [];
+var country = [];
+var data2 = [];
+
+dbRefObject.on('value', snap => {
+    preObject.innerText = JSON.stringify(snap.val(),null,3);
+    data = snap.val();
+    console.log('aaaaa',data);
+
+
+    for (i in data){
+      data2 = data[i];
+      ad_name.push(data2.name);
+      icao.push(data2.icao)
+      iata.push(data2.iata)
+      elev.push(data2.elevation)
+      country.push(data2.country)
+      type.push(data2.type)
+      coor.push(data2.coor)
+    }
+    console.log('ad',ad_name);
+    console.log('coor',coor);
+
+});
+
+dbRefList.on('child_added', snap => {
+    const li = document.createElement('li');
+    li.innerText = snap.val();
+    li.id = snap.key;
+    ulList.appendChild(li);
+}); // checkt als er iets bijkomt in de aerodromes lijst op firebase db
+dbRefList.on('child_changed',snap => {
+  const liChanged = document.getElementById(snap.key);
+  liChanged.innerText = snap.val();
+});
+dbRefList.on('child_removed',snap => {
+  const liToRemove = document.getElementById(snap.key);
+  liToRemove.remove();
+});
+
+function get_Array (name){
+  if (name == 'icao'){
+    //console.log('icao',icao)
+    return icao
+  }
+}
+
+get_Array('icao')
