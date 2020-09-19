f44aae6b3685c501de796c6075853aa1352ce138
Denivros <45978934+Denivros@users.noreply.github.com>
2020-09-19 14:56:30 +0200

    Update index.js

--- frontend/guivue/src/scripts/index.js
+++ frontend/guivue/src/scripts/index.js
@@ -13,58 +13,58 @@ var fire = firebase.initializeApp({
 });
 //firebase.analytics();
 
-export default fire;
+// export default fire;
 
-// const preObject = document.getElementById('object');
-// const ulList = document.getElementById('list');
-// // Get a reference to the database service
-// const dbRefObject = firebase.database().ref().child('Aerodromes');
-// const dbRefList = dbRefObject.child('Aerodromes');
-
-
-// var ad_name = [];
-// var icao = [];
-// var iata = [];
-// var coor = [];
-// var elev = [];
-// var type = [];
-// var country = [];
-// var data2 = [];
-
-// dbRefObject.on('value', snap => {
-//     preObject.innerText = JSON.stringify(snap.val(),null,3);
-//     data = snap.val();
-//     console.log('aaaaa',data);
+const preObject = document.getElementById('object');
+const ulList = document.getElementById('list');
+// Get a reference to the database service
+const dbRefObject = firebase.database().ref().child('Aerodromes');
+const dbRefList = dbRefObject.child('Aerodromes');
 
 
-//     for (i in data){
-//       data2 = data[i];
-//       ad_name.push(data2.name);
-//       icao.push(data2.icao)
-//       iata.push(data2.iata)
-//       elev.push(data2.elevation)
-//       country.push(data2.country)
-//       type.push(data2.type)
-//       coor.push(data2.coor)
-//     }
-//     console.log('ad',ad_name);
-//     console.log('coor',coor);
+var ad_name = [];
+var icao = [];
+var iata = [];
+var coor = [];
+var elev = [];
+var type = [];
+var country = [];
+var data2 = [];
 
-// });
+dbRefObject.on('value', snap => {
+    preObject.innerText = JSON.stringify(snap.val(),null,3);
+    data = snap.val();
+    console.log('aaaaa',data);
 
-// dbRefList.on('child_added', snap => {
-//     const li = document.createElement('li');
-//     li.innerText = snap.val();
-//     li.id = snap.key;
-//     ulList.appendChild(li);
-// }); // checkt als er iets bijkomt in de aerodromes lijst op firebase db
-// dbRefList.on('child_changed',snap => {
-//   const liChanged = document.getElementById(snap.key);
-//   liChanged.innerText = snap.val();
-// });
-// dbRefList.on('child_removed',snap => {
-//   const liToRemove = document.getElementById(snap.key);
-//   liToRemove.remove();
-// });
 
-// // export default icao;
+    for (i in data){
+      data2 = data[i];
+      ad_name.push(data2.name);
+      icao.push(data2.icao)
+      iata.push(data2.iata)
+      elev.push(data2.elevation)
+      country.push(data2.country)
+      type.push(data2.type)
+      coor.push(data2.coor)
+    }
+    console.log('ad',ad_name);
+    console.log('coor',coor);
+
+});
+
+dbRefList.on('child_added', snap => {
+    const li = document.createElement('li');
+    li.innerText = snap.val();
+    li.id = snap.key;
+    ulList.appendChild(li);
+}); // checkt als er iets bijkomt in de aerodromes lijst op firebase db
+dbRefList.on('child_changed',snap => {
+  const liChanged = document.getElementById(snap.key);
+  liChanged.innerText = snap.val();
+});
+dbRefList.on('child_removed',snap => {
+  const liToRemove = document.getElementById(snap.key);
+  liToRemove.remove();
+});
+
+export default icao;
